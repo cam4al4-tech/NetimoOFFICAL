b43069019a1df751246a6de0f229baa6a81359c1
cam4al4-tech <cam4al4@gmail.com>
2026-04-04 12:50:36 +0400

    Add files via upload

---
+++ 0001A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        long long x, y;
+        cin >> x >> y;
+        if (x == y) {
+            cout << -1 << "\n";
+        }
+        else if (y>x) {
+            cout << 2 << "\n";
+        } else {
+            cout << 3 << "\n";
+        }}
+
+    return 0;
+
+}
